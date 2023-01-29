5155ae7499a07dbcfc9ab43c704e2e1ad6f7e2f1
mapro84 <mapro84@gmail.com>
2023-01-29 21:10:57 +0100

    .

--- src/app/Controller/BOController.php
+++ src/app/Controller/BOController.php
@@ -8,10 +8,15 @@ use src\app\Controller\UserController;
 class BOController extends AppController{
 	
 	public function show(array $messages=[]) {
-		$skills = Entity::getAll('skill');
-		$items = Entity::getAll('item');
-		$entities = array('skills' => $skills, 'items' => $items, 'messages' => $messages);
-		$this->render('bo',$entities);
+        if (getenv('admin') !== 'true'){
+            $userController = new \src\app\Controller\UserController();
+            $userController->login();
+        }else{
+            $skills = Entity::getAll('skill');
+            $items = Entity::getAll('item');
+            $entities = array('skills' => $skills, 'items' => $items, 'messages' => $messages);
+            $this->render('bo',$entities);
+        }
 	}
 
 }
