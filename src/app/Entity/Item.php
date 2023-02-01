745653ac148e55823fb7d6d8aab53e9002e846b5
mapro84 <mapro84@gmail.com>
2023-02-01 21:52:02 +0100

    .

--- src/app/Entity/Item.php
+++ src/app/Entity/Item.php
@@ -22,7 +22,7 @@ class Item extends Entity{
         FROM item as i 
         LEFT OUTER JOIN skill as s ON i.skill_id = s.id
         WHERE MATCH(i.name,i.description,i.further) against(?)
-        OR MATCH(s.name) against(?);';
+        OR MATCH(s.name) against(?) ORDER BY i.name;';
         $queryParams = [$search, $search];
         return DB::prepare($query, $queryParams);
     }
@@ -32,7 +32,7 @@ class Item extends Entity{
         s.name as skillname, s.logo, s.id as skill_id
         FROM item as i 
         LEFT OUTER JOIN skill as s ON i.skill_id = s.id
-        WHERE i.skill_id = ? ;';
+        WHERE i.skill_id = ?  ORDER BY i.name;';
         $queryParams = [$skill_id];
         return DB::prepare($query, $queryParams);
     }
